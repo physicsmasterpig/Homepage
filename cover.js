2911130d7b9ce1517203467fa6d573bcb9e82428
Soo-Chang-Lee <physicsmp1217@snu.ac.kr>
2023-05-26 13:38:00 +0900

    2023.05.26

--- cover.js
+++ cover.js
@@ -7,23 +7,24 @@ const rbody = document.querySelector("body");
 
 window.addEventListener("click", function(e) {
  if(e.target.classList.contains('cover-page')){
-      coverPage.style.height = "0%";
-      body.style.marginTop = "0vh";
       covertext.style.visibility ='hidden';   
       welcome.style.visibility ='hidden';
       body.style.visibility = 'visible'; 
       body.style.overflow = 'auto';
       rbody.style.overflow = 'auto';
+      body.style.marginTop = "0%";
+      document.documentElement.scrollTop = 0;
     }}
 )
 
 window.addEventListener("keypress", function(e){
-    if(coverPage.style.height != '0%'){
-      coverPage.style.height = "0%";
-      body.style.marginTop = "0vh";
+    if(coverPage.style.visibility != 'hidden'){
       covertext.style.visibility ='hidden';   
       welcome.style.visibility ='hidden';
-      body.style.visibility = 'visible';
+      body.style.visibility = 'visible'; 
+      body.style.overflow = 'auto';
       rbody.style.overflow = 'auto';
+      body.style.marginTop = "0%";
+      document.documentElement.scrollTop = 0;
   }
 })
